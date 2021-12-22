23bd63274a804a2df03e9caaa714c1adb9864371
Mihkel Lember <milember@tktk.ee>
2021-12-22 04:09:03 +0200

    OOPAndFiles

---
+++ Frozen/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frozen
+{
+    internal class Program
+    {
+        class person
+        {
+            string character;
+            string gift;
+            
+            public person(string _character, string _gift)
+            {
+                character = _character;
+                gift = _gift;
+                
+            }
+
+            public string Character
+            {
+                get { return character; }
+            }
+
+            public string Gift
+            {
+                get { return gift; }
+            }
+                        
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<person> myPersons = new List<person>();
+            string[] personsFromFile = GetDataFromFile();
+
+            foreach (string line in personsFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                person newPerson = new person(tempArray[0], tempArray[1]);
+                myPersons.Add(newPerson);
+
+            }
+
+            foreach (person personFromList in myPersons)
+            {
+                Console.WriteLine($"{personFromList.Character} wants {personFromList.Gift}.");
+            }
+
+        }
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach (string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\lembe\samples\frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+}
---
+++ OOPAndFiles/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAndFiles
+{
+    internal class Program
+    {
+        class movie
+        {
+            string title;
+            string rating;
+            string year;
+
+            public movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+
+            }
+
+            public string Title
+            {
+                get { return title; }
+            }
+
+            public string Rating
+            {
+                get { return rating; }
+            }
+
+            public string Year
+            {
+                get { return year; }
+            }
+        }
+                    
+
+        static void Main(string[] args)
+        {
+            List<movie> myMovies = new List<movie>();
+            string[] moviesFromFile = GetDataFromFile();
+
+            foreach(string line in moviesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+                movie newMovie =new movie(tempArray[0], tempArray[1], tempArray[2]);
+                myMovies.Add(newMovie);
+
+            }
+
+            foreach(movie movieFromList in myMovies)
+            {
+                Console.WriteLine($"Title --> {movieFromList.Title}. Rating --> {movieFromList.Rating}. Year --> {movieFromList.Year}"); 
+            }
+
+        }
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach(string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\lembe\samples\movies.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+        
+}
